7d97b9525a0f2943fae4a5872e81e4e1485585b9
PawelTo <pawel.toporowicz@gmail.com>
2020-07-31 22:28:48 +0200

    setup Oidc client

---
+++ src/main/java/app/configuration/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2ClientConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@Profile("oidc-client")
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //.addFilterAfter()
+                .csrf().disable()
+                //.formLogin().disable()
+                .httpBasic().disable()
+                .logout().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                //.requestMatchers(unsecuredRequestMatcher()).permitAll()
+                .antMatchers("/**").authenticated()
+                .anyRequest().denyAll()
+                .and()
+                .oauth2Client(OAuth2ClientConfigurer::authorizationCodeGrant)
+        ;
+    }
+}
